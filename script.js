a7abc9f33b459f7a7184af31666bcc0f0710fc54
Memmedova <memmedovaperiii@gmail.com>
2026-02-16 17:04:30 +0400

    Changes attached.

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
-const languageSwitcher = document.getElementById('language-switcher');
-let translations = {};
+// const languageSwitcher = document.getElementById('language-switcher');
+// let translations = {};
 
-async function setLanguage(newLocale) {
-  localStorage.setItem('language', newLocale);
-  await fetchTranslations(newLocale);
-  translatePage();
-}
+// async function setLanguage(newLocale) {
+//   localStorage.setItem('language', newLocale);
+//   await fetchTranslations(newLocale);
+//   translatePage();
+// }
 
-async function fetchTranslations(newLocale) {
-  const response = await fetch(`./lang/${newLocale}.json`);
-  translations = await response.json();
-}
+// async function fetchTranslations(newLocale) {
+//   const response = await fetch(`./lang/${newLocale}.json`);
+//   translations = await response.json();
+// }
 
-function translatePage() {
-  document.querySelectorAll('[data-i18n-key]').forEach(element => {
-    const key = element.getAttribute('data-i18n-key');
-    if (translations[key]) {
-      element.textContent = translations[key];
-    }
-  });
-}
+// function translatePage() {
+//   document.querySelectorAll('[data-i18n-key]').forEach(element => {
+//     const key = element.getAttribute('data-i18n-key');
+//     if (translations[key]) {
+//       element.textContent = translations[key];
+//     }
+//   });
+// }
 
-languageSwitcher.addEventListener('change', (event) => {
-  setLanguage(event.target.value);
-});
+// languageSwitcher.addEventListener('change', (event) => {
+//   setLanguage(event.target.value);
+// });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const userLocale = localStorage.getItem('language') || 'en';
-  languageSwitcher.value = userLocale;
-  setLanguage(userLocale);
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//   const userLocale = localStorage.getItem('language') || 'en';
+//   languageSwitcher.value = userLocale;
+//   setLanguage(userLocale);
+// });
+const form = document.querySelector("form");
+const submitSignup = (e) => {
+  e.preventDefault();
+};
+form.addEventListener("submit", submitSignup);
